b5b2cf3416fee2357cf373f2b1240cbadd78fc93
milk9111 <connorlndbrg349@gmail.com>
2018-02-17 12:20:34 -0800

    Connected to postgresql database.

--- php/gettweetsforuser.php
+++ php/gettweetsforuser.php
@@ -1,5 +1,15 @@
 <?php
-    include ("../credentials.php");
 
-    $user = $_GET['user'];
-    echo "monkey butt";
+include ("../credentials.php");
+
+$user = $_GET['user'];
+try {
+    $conn = new PDO("pgsql:host=$hostname;port=5432;dbname=$dbname2;user=$username;password=$password");
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
